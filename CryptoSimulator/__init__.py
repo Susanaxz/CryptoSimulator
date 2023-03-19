b4e186bd17bdaea8abb47322320d038b9a3daa06
Susana_xz <riberasusana@gmail.com>
2023-03-19 09:18:24 +0100

    Revert "feat: añadimos Cors a requirements.txt"
    This reverts commit adf98c2ae3001e411e9aee6ac70e178337696024.

--- CryptoSimulator/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-# from flask_cors import CORS
-
-app = Flask(__name__)
-
-app.config.from_object('config') # importa la configuración de la aplicación desde el archivo config.py
-# cors = CORS(app, resources={r"/api/*": {"origins": "*"}}) # permite que cualquier origen pueda acceder a la API REST (CORS)
