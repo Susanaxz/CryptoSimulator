19e6c67eb7ce86994d9eedae9de75cba7b8b7757
Susana_xz <riberasusana@gmail.com>
2023-03-21 09:46:40 +0100

    feat: añado el listado de monedas con las que se operará en config.py

--- config_sample.py
+++ config_sample.py
@@ -3,3 +3,11 @@ import os
 RUTA = os.path.join('data', 'transactions.db')
 SECRET_KEY ='por aquí tu secret key'
 APIKEY= 'pon aquí tu api key de coinAPI'
+
+# preparar la paginación
+DEFAULT_PAG = 1
+PAG_SIZE = 10
+
+
+# listado de monedas
+COINS = ['EUR', 'BTC', 'ETH', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'DOGE', 'SHIB']
