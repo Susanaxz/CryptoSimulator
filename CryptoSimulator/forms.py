a480c56d36349a97b6b7513f1b644c47aa56278b
Susana_xz <riberasusana@gmail.com>
2023-03-30 15:52:26 +0200

    style: cambio de texto en formulario

--- CryptoSimulator/forms.py
+++ CryptoSimulator/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired
 class TransactionForm(FlaskForm):
     id = IntegerField(default=0, widget=HiddenInput())
     from_currency = StringField('Moneda origen', validators=[DataRequired(message="Debes introducir una moneda origen")])
-    from_quantity = FloatField('Cantidad en Euros', validators=[DataRequired(message="Debes introducir una cantidad en Euros")])
+    from_quantity = FloatField('Cantidad origen', validators=[DataRequired(message="Debes introducir una cantidad")])
     to_currency = StringField('Moneda destino', validators=[DataRequired(message="Debes introducir una moneda destino")])
     submit = SubmitField('Realizar transacción')
